f1e6cbdb098b96467b4e53624a71ee5dca88eade
mh120888 <matthew.alan.higgins@gmail.com>
2014-03-08 15:03:12 -0800

    Calculates 15% tip for a given total bill amount, displays it on index page.

--- tips.js
+++ tips.js
@@ -1,15 +1,39 @@
 $( document ).ready(function() {
-  Bill.bindEvents();
+  BillCalculations.bindEvents();
 });
 
-var Bill = {
+// var BillController = {
+//   bindEvents: function() {
+//     $("#bill").submit(BillCalculations.calculateBillAndTip);
+//   },
+//   calculateAndDisplay: function() {
+//     event.preventDefault();
+//   }
+// }
+
+var BillCalculations = {
+  tip: 0,
   billTotal: 0,
+  tipPercentage: 0.15,
   bindEvents: function() {
-    $("#bill").submit(Bill.serializeInput);
+    $("#bill").submit(BillCalculations.calculateBillAndTip);
   },
-  serializeInput: function() {
+  calculateBillAndTip: function() {
     event.preventDefault();
-    billTotal = $('input[name="total"]').val();
-    debugger;
+    BillCalculations.assignBillTotal();
+    BillCalculations.addTip();
+    BillViewer.displayBillWithTip();
   },
+  assignBillTotal: function() {
+    BillCalculations.billTotal = parseFloat($('input[name="total"]').val());
+  },
+  addTip: function() {
+    BillCalculations.billTotal = BillCalculations.billTotal * (1 + BillCalculations.tipPercentage);
+  }
 };
+
+var BillViewer = {
+  displayBillWithTip: function() {
+    $('#bill-with-tip').html("Bill w/tip: $ " + BillCalculations.billTotal.toFixed(2));
+  }
+}
